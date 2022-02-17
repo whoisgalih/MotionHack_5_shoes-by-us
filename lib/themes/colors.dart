18c707602dc96ca79d701e5d31872f35fefc5ffb
Galih Akbar <galihakbar.ga91@gmail.com>
2022-02-17 10:58:38 +0700

    initial commit

---
+++ lib/themes/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// Primary
+Color primary50 = const Color(0xFF2AF56F);
+
+// Secondary
+Color secondary50 = const Color(0xFF252525);
+
+// Semantic
+Color green1 = const Color(0xFFA9FFB2);
+Color green2 = const Color(0xFF3E9141);
+Color red1 = const Color(0xFFFFA9A9);
+Color red2 = const Color(0xFF913E3E);
+Color yellow1 = const Color(0xFFFFF1A9);
+Color yellow2 = const Color(0xFF91843E);
+Color blue1 = const Color(0xFFA9DBFF);
+Color blue2 = const Color(0xFF3E5091);
+
+// Neutral
+Color neutral1 = secondary50;
+Color neutral2 = const Color(0xFFB8D5EA);
+Color neutral3 = const Color(0xFFEEEEEE);
+Color neutral4 = const Color(0xFFFFFFFF);
